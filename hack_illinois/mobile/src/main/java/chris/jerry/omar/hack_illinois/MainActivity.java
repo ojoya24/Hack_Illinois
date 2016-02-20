871cc45e6f827159a5689658a22afff75972e0ee
Omar Joya <ojoya24@gmail.com>
2016-02-20 09:59:11 -0600

    Last Night

--- hack_illinois/mobile/src/main/java/chris/jerry/omar/hack_illinois/MainActivity.java
+++ hack_illinois/mobile/src/main/java/chris/jerry/omar/hack_illinois/MainActivity.java
@@ -1,9 +1,17 @@
 package chris.jerry.omar.hack_illinois;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +20,65 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //check for Bluetooth capabilities
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null){
+            //device does not have bluetooth
+        }
+        if (!mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        //retrieve the bluetooth device device to communicate with
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0){
+            for (BluetoothDevice device : pairedDevices){
+                BluetoothDevice mDevice = device;
+            }
+        }
+
     }
 
+    //Create a new thread since this can take a good amount of time. Here we form the connection
+    //between Android and Arduino.
+    private class ConnectThread extends Thread{
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        public ConnectThread(BluetoothDevice device){
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {}
+            mmSocket = tmp;
+        }
+
+        public void run(){
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            mBluetoothAdapter.cancelDiscovery();
+            try {
+                mmSocket.connect();
+            } catch (IOException connectException){
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException){}
+                return;
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e){}
+        }
+    }
+
+
+    //We are not concerned with the section below------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
