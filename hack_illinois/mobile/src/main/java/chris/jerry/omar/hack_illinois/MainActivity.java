b968a76e9f3d4435e5b52ab6269f858656b48570
Omar Joya <ojoya24@gmail.com>
2016-02-20 11:40:58 -0600

    Morning

--- hack_illinois/mobile/src/main/java/chris/jerry/omar/hack_illinois/MainActivity.java
+++ hack_illinois/mobile/src/main/java/chris/jerry/omar/hack_illinois/MainActivity.java
@@ -6,8 +6,11 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import java.io.IOException;
 import java.util.Set;
@@ -15,6 +18,16 @@ import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //declare the switch widget you're using
+    Switch ledSwitch = (Switch) findViewById(R.id.led_switch);
+    ledSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()){
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+            Log.v("Switch State=", "" + isChecked);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +37,49 @@ public class MainActivity extends ActionBarActivity {
         //check for Bluetooth capabilities
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null){
-            //device does not have bluetooth
+            finish();
         }
         if (!mBluetoothAdapter.isEnabled()){
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 1);
         }
 
-        //retrieve the bluetooth device device to communicate with
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0){
-            for (BluetoothDevice device : pairedDevices){
-                BluetoothDevice mDevice = device;
-            }
-        }
+       // ConnectThread mConnectThread = new ConnectThread(mDevice);
+        //mConnectThread.start();
 
     }
 
+    //We are not concerned with the section below------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    //We are not concerned with the section above--------------------------------
+
     //Create a new thread since this can take a good amount of time. Here we form the connection
     //between Android and Arduino.
-    private class ConnectThread extends Thread{
+    /*private class ConnectThread extends Thread{
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
         private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
@@ -75,29 +111,5 @@ public class MainActivity extends ActionBarActivity {
                 mmSocket.close();
             } catch (IOException e){}
         }
-    }
-
-
-    //We are not concerned with the section below------------------------------
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
